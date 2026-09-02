17e7150e5cb729785aff62855fc48a28fb36e1c1
Ishimwe Tresor <ishimwet15@gmail.com>
2026-09-02 19:26:23 +0200

    fixing location

---
+++ tests/regression/login-negative.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('user cannot login with invalid username', async ({ page }) => {
+
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.getByPlaceholder('Username').fill('wrong_user');
+
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByText('Epic sadface')).toBeVisible();
+});
