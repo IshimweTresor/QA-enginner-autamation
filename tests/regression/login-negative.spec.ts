4f1e788226bbbf9472aa057af5abe01228007ce8
Ishimwe Tresor <ishimwet15@gmail.com>
2026-09-02 19:39:09 +0200

    invalid password

--- tests/regression/login-negative.spec.ts
+++ tests/regression/login-negative.spec.ts
@@ -12,3 +12,16 @@ test('user cannot login with invalid username', async ({ page }) => {
 
   await expect(page.getByText('Epic sadface')).toBeVisible();
 });
+
+test('user cannot login with invalid password', async ({ page }) => {
+
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.getByPlaceholder('Username').fill('standard_user');
+
+  await page.getByPlaceholder('Password').fill('wrong_password');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByText('Epic sadface')).toBeVisible();
+});
