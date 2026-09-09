d768d926813c3d829418b8b841dbeae6f5abc2b6
Ishimwe Tresor <ishimwet15@gmail.com>
2026-09-09 19:39:37 +0200

    before each

--- tests/regression/login-negative.spec.ts
+++ tests/regression/login-negative.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+});
+
 test('user cannot login with invalid username', async ({ page }) => {
 
-  await page.goto('https://www.saucedemo.com/');
-
   await page.getByPlaceholder('Username').fill('wrong_user');
-
   await page.getByPlaceholder('Password').fill('secret_sauce');
 
   await page.getByRole('button', { name: 'Login' }).click();
@@ -15,10 +16,7 @@ test('user cannot login with invalid username', async ({ page }) => {
 
 test('user cannot login with invalid password', async ({ page }) => {
 
-  await page.goto('https://www.saucedemo.com/');
-
   await page.getByPlaceholder('Username').fill('standard_user');
-
   await page.getByPlaceholder('Password').fill('wrong_password');
 
   await page.getByRole('button', { name: 'Login' }).click();
@@ -28,10 +26,7 @@ test('user cannot login with invalid password', async ({ page }) => {
 
 test('user cannot login with invalid username and password', async ({ page }) => {
 
-  await page.goto('https://www.saucedemo.com/');
-
   await page.getByPlaceholder('Username').fill('wrong_user');
-
   await page.getByPlaceholder('Password').fill('wrong_password');
 
   await page.getByRole('button', { name: 'Login' }).click();
@@ -41,11 +36,18 @@ test('user cannot login with invalid username and password', async ({ page }) =>
 
 test('user cannot login with empty username', async ({ page }) => {
 
-  await page.goto('https://www.saucedemo.com/');
-
   await page.getByPlaceholder('Password').fill('secret_sauce');
 
   await page.getByRole('button', { name: 'Login' }).click();
 
   await expect(page.getByText('Username is required')).toBeVisible();
+});
+
+test('user cannot login with empty password', async ({ page }) => {
+
+  await page.getByPlaceholder('Username').fill('standard_user');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByText('Password is required')).toBeVisible();
 });
